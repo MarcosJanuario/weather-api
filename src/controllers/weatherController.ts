5d8c94b8149f69e89595d7ca888fd0452221792b
Marcos Januário <januario.marcos@icloud.com>
2023-11-29 14:47:33 +0100

    proper error handling in weatherController

--- src/controllers/weatherController.ts
+++ src/controllers/weatherController.ts
@@ -1,46 +1,52 @@
 import { Request, Response } from 'express';
 import { getWeather } from '../services/weatherService';
-import { CurrentWeather } from '../types/weather';
+import { WeatherResponse, CurrentWeather } from '../types/weather';
 
-export default async function getWeatherData(req: Request, res: Response): Promise<void> {
-  const cityName: string = req.body.cityName;
-
+export default async function getWeatherData(req: Request, res: Response): Promise<void | Response<any>> {
   try {
-    const weatherData = await getWeather(cityName);
+    const cityName: string = req.body.cityName;
+    if (!cityName) {
+      return res.status(400).json({ error: 'CityName not provided.' });
+    }
 
-    const currentWeather: CurrentWeather = {
-      observationTime: weatherData.current.observation_time,
-      temperature: weatherData.current.temperature,
-      weatherCode: weatherData.current.weather_code,
-      weatherIcons: weatherData.current.weather_icons,
-      weatherDescriptions: weatherData.current.weather_descriptions,
-      windSpeed: weatherData.current.wind_speed,
-      windDegree: weatherData.current.wind_degree,
-      windDir: weatherData.current.wind_dir,
-      pressure: weatherData.current.pressure,
-      precipitation: weatherData.current.precip,
-      humidity: weatherData.current.humidity,
-      cloudCover: weatherData.current.cloudcover,
-      feelsLike: weatherData.current.feelslike,
-      uvIndex: weatherData.current.uv_index,
-      visibility: weatherData.current.visibility,
-      location: {
-        name: weatherData.location.name,
-        country: weatherData.location.country,
-        region: weatherData.location.region,
-        lat: weatherData.location.lat,
-        lon: weatherData.location.lon,
-        timezoneId: weatherData.location.timezone_id,
-        localtime: weatherData.location.localtime,
-        localtimeEpoch: weatherData.location.localtime_epoch,
-        utcOffset: weatherData.location.utc_offset,
-      },
-    };
+    const weatherData: WeatherResponse = await getWeather(cityName);
 
-    res.json(currentWeather);
+    if (weatherData.current) {
+      const currentWeather: CurrentWeather = {
+        observationTime: weatherData.current.observation_time,
+        temperature: weatherData.current.temperature,
+        weatherCode: weatherData.current.weather_code,
+        weatherIcons: weatherData.current.weather_icons,
+        weatherDescriptions: weatherData.current.weather_descriptions,
+        windSpeed: weatherData.current.wind_speed,
+        windDegree: weatherData.current.wind_degree,
+        windDir: weatherData.current.wind_dir,
+        pressure: weatherData.current.pressure,
+        precipitation: weatherData.current.precip,
+        humidity: weatherData.current.humidity,
+        cloudCover: weatherData.current.cloudcover,
+        feelsLike: weatherData.current.feelslike,
+        uvIndex: weatherData.current.uv_index,
+        visibility: weatherData.current.visibility,
+        location: {
+          name: weatherData.location.name,
+          country: weatherData.location.country,
+          region: weatherData.location.region,
+          lat: weatherData.location.lat,
+          lon: weatherData.location.lon,
+          timezoneId: weatherData.location.timezone_id,
+          localtime: weatherData.location.localtime,
+          localtimeEpoch: weatherData.location.localtime_epoch,
+          utcOffset: weatherData.location.utc_offset,
+        },
+      };
+
+      return res.json(currentWeather);
+    } else {
+      return res.status(400).json({ error: 'Invalid city name' });
+    }
   } catch (error) {
-    //TODO: improve error handler
     console.error(error);
-    res.status(500).send('Error fetching weather data');
+    return res.status(500).json({ error: 'Internal Server Error' });
   }
 }
