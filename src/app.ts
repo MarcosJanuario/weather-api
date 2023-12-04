018912c7e6fc0106ca45c3a8d4b7b2059c014cab
Marcos Januário <januario.marcos@icloud.com>
2023-12-04 06:30:04 +0100

    adjusted listening port

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import express from 'express';
 import cors from 'cors';
 import routes from './routes';
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
